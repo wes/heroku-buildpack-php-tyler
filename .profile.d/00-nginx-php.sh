0af509b1523d381fd2a75cdc2217fa33d9788344
Ronald Ip <myself@iphoting.com>
2013-08-18 09:16:15 +0800

    Also set $LD_LIBRARY_PATH via .profile.d mechanism.

--- .profile.d/00-nginx-php.sh
+++ .profile.d/00-nginx-php.sh
@@ -1,2 +1,3 @@
 # Set default buildpack path.
 export PATH="$PATH:/app/vendor/bin:/app/local/bin:/app/vendor/nginx/sbin:/app/vendor/php/bin:/app/vendor/php/sbin:/usr/local/bin:/usr/bin:/bin"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/app/local/lib"
