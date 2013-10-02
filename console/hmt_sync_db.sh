1f8f65b2ca4c8ffbcf7b4130247c2e0bb94c54d6
Wes <wes@holdmyticket.com>
2013-10-02 13:34:12 -0600

    fixing console

--- console/hmt_sync_db.sh
+++ console/hmt_sync_db.sh
@@ -2,7 +2,7 @@ echo 'Dropping Local Database'
 mysqladmin -u root -proot drop holdmyticket
 
 echo 'Dumping Live Read Replica DB To File'
-mysqldump -h holdmyticket.ctycy1cg9bsq.us-east-1.rds.amazonaws.com -P 3306 -u holdmyticket -p /tmp/holdmyticket.sql
+mysqldump -h holdmyticket.ctycy1cg9bsq.us-east-1.rds.amazonaws.com -P 3306 -u holdmyticket -p holdmyticket > /tmp/holdmyticket.sql
 
 echo 'Creating Local Database'
 mysqladmin -u root -proot create holdmyticket
