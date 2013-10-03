1c304ac9a95a0a625017edcb919937f5e64e400d
Wes <wes@holdmyticket.com>
2013-10-03 15:48:26 -0600

    go

--- console/hmt_sync_db.sh
+++ console/hmt_sync_db.sh
@@ -1,16 +1,19 @@
 echo 'Dropping Local Database'
 mysqladmin -u root -proot drop holdmyticket
 
+echo 'Creating tmp folder in vagrant folder to save space.'
+mkdir /vagrant/.tmp/
+
 echo 'Dumping Live Read Replica DB To File'
-mysqldump -h hmtread.ctycy1cg9bsq.us-east-1.rds.amazonaws.com -P 3306 -u holdmyticket -p holdmyticket > /tmp/holdmyticket.sql
+mysqldump -h hmtread.ctycy1cg9bsq.us-east-1.rds.amazonaws.com -P 3306 -u holdmyticket -p holdmyticket > /vagrant/.tmp/holdmyticket.sql
 
 echo 'Creating Local Database'
 mysqladmin -u root -proot create holdmyticket
 
 echo 'Importing Database'
-mysql -u root -proot holdmyticket < /tmp/holdmyticket.sql
+mysql -u root -proot holdmyticket < /vagrant/.tmp/holdmyticket.sql
 
 echo 'Cleaning up'
-rm /tmp/holdmyticket.sql
+rm -rf /vagrant/.tmp/
 
 echo 'All Done'
