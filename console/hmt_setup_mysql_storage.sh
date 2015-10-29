e85c9238f78fb8885378af7d548fe0b392b37795
Wes <wes@holdmyticket.com>
2015-10-29 10:30:28 -0600

    more updates to vagrant setup

--- console/hmt_setup_mysql_storage.sh
+++ console/hmt_setup_mysql_storage.sh
@@ -5,8 +5,4 @@ chown -R mysql.mysql /db
 chmod -R g+w /db
 /etc/init.d/mysql.server start
 
-mysql -u root -proot -h localhost mysql < "update user SET Host = '10.0.0.1' WHERE Host = '127.0.0.1';"
-
-mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root -p mysql
-
 echo "You will now need to reload vagrant again."
