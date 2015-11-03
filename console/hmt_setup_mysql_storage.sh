51a4bd38443a0698aef75aa9ad2bb2cd5c66bd68
Wes <wes@holdmyticket.com>
2015-11-03 16:34:01 -0700

    fix of mysql cnf

--- console/hmt_setup_mysql_storage.sh
+++ console/hmt_setup_mysql_storage.sh
@@ -1,7 +1,7 @@
 /etc/init.d/mysql stop
 mv /var/lib/mysql/* /db/
 sed -i "s,datadir.*=.*,datadir=/db,g" /etc/mysql/my.cnf
-sed -e '/bind-address/ s/^#*/#/' -i /etc/mysql/my.cnf
+sed -i "s,bind-address.*=.*,bind-address=10.0.0.2,g" /etc/mysql/my.cnf
 chown -R mysql.mysql /db
 /etc/init.d/mysql start
 
