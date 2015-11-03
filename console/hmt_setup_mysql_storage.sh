5f1d42e31beeed633eb231f5ca7e1d2b6a7a8f74
Wes <wes@holdmyticket.com>
2015-11-02 18:58:01 -0700

    fixes to mysql storage script

--- console/hmt_setup_mysql_storage.sh
+++ console/hmt_setup_mysql_storage.sh
@@ -1,9 +1,10 @@
 /etc/init.d/mysql stop
 mv /var/lib/mysql/* /db/
-rm -rf /var/lib/mysql/data
-ln -s /db /var/lib/mysql/data
-chown -R mysql.mysql /db
-chmod -R g+w /db
-/etc/init.d/mysql.server start
+#rm -rf /var/lib/mysql/data
+#ln -s /db /var/lib/mysql/data
+#chown -R mysql.mysql /db
+#chmod -R g+w /db
+sed -i "s,datadir.*=.*,datadir=/db,g" /etc/mysql/my.cnf
+/etc/init.d/mysql start
 
 echo "You will now need to reload vagrant again."
