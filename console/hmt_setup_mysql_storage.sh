1f327dda6e8b5eefba1ef4c87bcb282a965b08ed
Wes <wes@holdmyticket.com>
2015-11-03 14:59:34 -0700

    adding a permission fix to a step

--- console/hmt_setup_mysql_storage.sh
+++ console/hmt_setup_mysql_storage.sh
@@ -2,6 +2,7 @@
 mv /var/lib/mysql/* /db/
 sed -i "s,datadir.*=.*,datadir=/db,g" /etc/mysql/my.cnf
 sed -e '/bind-address/ s/^#*/#/' -i /etc/mysql/my.cnf
+chown -R mysql.mysql /db
 /etc/init.d/mysql start
 
 echo "You will now need to reload vagrant again."
