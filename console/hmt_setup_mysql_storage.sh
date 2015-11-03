389c2a33f2f979703a1b13409ed91b9abd1f3edf
Wes <wes@holdmyticket.com>
2015-11-02 19:27:57 -0700

    making mysql bind on localhost to give access from outside vagrant

--- console/hmt_setup_mysql_storage.sh
+++ console/hmt_setup_mysql_storage.sh
@@ -1,10 +1,7 @@
 /etc/init.d/mysql stop
 mv /var/lib/mysql/* /db/
-#rm -rf /var/lib/mysql/data
-#ln -s /db /var/lib/mysql/data
-#chown -R mysql.mysql /db
-#chmod -R g+w /db
 sed -i "s,datadir.*=.*,datadir=/db,g" /etc/mysql/my.cnf
+sed -e '/bind-address/ s/^#*/#/' -i /etc/mysql/my.cnf
 /etc/init.d/mysql start
 
 echo "You will now need to reload vagrant again."
