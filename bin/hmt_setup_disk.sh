5b9869f599817d94cccc275b59919e9c20858cd1
Wes <wes@holdmyticket.com>
2015-11-03 23:12:10 -0700

    disabling disk stuff. adding ssh key gen in the compiling

--- bin/hmt_setup_disk.sh
+++ bin/hmt_setup_disk.sh
@@ -1,6 +1,6 @@
-/etc/init.d/mysql.server stop
-fdisk /dev/sdb
-mkfs.ext4 /dev/sdb1
-mkdir /db
-echo "/dev/sdb1      /db      ext4      errors=remount-ro      0    1" >> /etc/fstab
-echo "You should now exit and reload vagrant."
+#/etc/init.d/mysql.server stop
+#fdisk /dev/sdb
+#mkfs.ext4 /dev/sdb1
+#mkdir /db
+#echo "/dev/sdb1      /db      ext4      errors=remount-ro      0    1" >> /etc/fstab
+#echo "You should now exit and reload vagrant."
--- bin/hmt_setup_mysql_storage.sh
+++ bin/hmt_setup_mysql_storage.sh
@@ -1,8 +1,8 @@
-/etc/init.d/mysql.server stop
-mv /usr/local/mysql/data/* /db/
+#/etc/init.d/mysql.server stop
+#mv /usr/local/mysql/data/* /db/
 #sed -i "s,datadir.*=.*,datadir=/db,g" /etc/mysql/my.cnf
 #sed -i "s,bind-address.*=.*,bind-address=10.0.0.2,g" /etc/mysql/my.cnf
-chown -R mysql.mysql /db
-/etc/init.d/mysql.server start
+#chown -R mysql.mysql /db
+#/etc/init.d/mysql.server start
 
 echo "You will now need to reload vagrant again."
